f431f3d6e404ccf81bc4f4c0a34e2ffceb31b911
Daniel Ramos <danielramosacosta@hotmail.com>
2019-05-19 21:29:09 +0100

    Add complexity example

---
+++ complexity.js
@@ -0,0 +1,11 @@
+/* eslint complexity: ["error", 2] */
+
+export function complex (x) {
+  if (true) {
+    return x
+  } else if (false) {
+    return x + 1
+  } else {
+    return 4 // 3rd path
+  }
+}
